89ac16a14b470c074a31bcab7c1a231f16e851c2
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-01-20 22:06:39 +0100

    dodanie tekstu

--- mobile-script.js
+++ mobile-script.js
@@ -5,10 +5,25 @@ const mobileNavUl = document.querySelector('.navUl');
 const mobileAboutMe = document.querySelector('.mobileAboutMe');
 const mobileTasks = document.querySelector('.mobileMyTasks');
 const mobileLinks = document.querySelector('.mobileLinks');
+const mobileAboutMeSection = document.querySelector('#mobileAboutMeSection')
+const mobileParaAboutMe = document.querySelector('.mobileParaAboutMe')
+const dots = '...'
+
+// read more creator
+let moreMobileParaAboutMe = document.createElement('p')
+        moreMobileParaAboutMe.classList.add('mobileParaAboutMe')
+        moreMobileParaAboutMe.style.margin = '0px'
+        mobileAboutMeSection.append(moreMobileParaAboutMe)
+// button creator
+const btnReadMore = document.createElement('button')
+mobileAboutMeSection.append(btnReadMore)
+btnReadMore.setAttribute('class', 'btnMore')
+btnReadMore.innerText = 'czytaj więcej'  
+
 
 const hideOpenMenu = e =>{
     if(e.target.matches('.show')){
-        mobileNavUl.classList.add('active');
+        mobileNavUl.classList.add('active')
         btnShowMenu.style.display = 'none'
         btncloseMenu.style.display = 'flex'
     }else if(e.target.matches('.hide')){
@@ -18,6 +33,18 @@ const hideOpenMenu = e =>{
     }
 }
 
+const showMoreTxt = (e) =>{
+    if(e.target.matches('.btnMore')){
+        moreMobileParaAboutMe.textContent = `Wiem, jak w życiu człowieka ważna jest determinacja i chęć 
+        dążenia do celu, nie boję się nowych wyzwań. Potrafię pracować w zespole, jestem otwarty i nastawiony na 
+        dłuższą współpracę. Ponadto znam język angielski w stopniu komunikatywnym, w tym słownictwo branżowe 
+        związane z programowaniem i obsługą komputera.`;
+        btnReadMore.innerText = 'czytaj mniej'
+        dots.textContent = ''
+    }
+}
+mobileParaAboutMe.textContent = `Jestem świadomy i w pełni przekonany, że chęć ciągłego rozwoju pozwoli mi osiągnąć wyznaczony przeze mnie cel, 
+czyli pracę na stanowisku Front-End Developera.${dots}${moreMobileParaAboutMe.textContent}`
 
 
 
@@ -27,9 +54,5 @@ const hideOpenMenu = e =>{
 
 
 
-
-
-
-
-
+mobileAboutMeSection.addEventListener('click', showMoreTxt)
 mobileNavBtnContener.addEventListener('click', hideOpenMenu)
