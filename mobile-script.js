88d6aac707c3f81043e60d97a038ba1d537c943c
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-01-20 14:38:17 +0100

    testy

---
+++ mobile-script.js
@@ -0,0 +1,9 @@
+const mobileNavBtnContener = document.querySelector('.burger-menu');
+const btnShowMenu = document.querySelector('.show');
+const btncloseMenu = document.querySelector('.hide');
+const mobileNavUl = document.querySelector('.navUl');
+const mobileAboutMe = document.querySelector('.mobileAboutMe');
+const mobileTasks = document.querySelector('.mobileMyTasks');
+const mobileLinks = document.querySelector('.mobileLinks');
+
+
