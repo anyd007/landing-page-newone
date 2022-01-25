011d85f1c560cb53080a52e6235153612bf089fa
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-01-25 14:06:43 +0100

    dodanie pliku do pobrania

--- mobile-script.js
+++ mobile-script.js
@@ -12,7 +12,7 @@ const dots = '...'
 const mobileLanguanges = document.querySelector('.mobileLanguanges');
 const cytat = document.querySelector('.cytat')
 const signature = document.querySelector('.signature')
-const closeFav = document.querySelector('.closeFav')
+const closeFav = document.querySelector('.closeFavSkills')
 
 // read more creator
 let moreMobileParaAboutMe = document.createElement('p')
@@ -70,29 +70,28 @@ const showMoreTxt = (e) =>{
      dłuższą współpracę. Ponadto znam język angielski w stopniu komunikatywnym, w tym słownictwo branżowe 
         związane z programowaniem i obsługą komputera. Jestem świadomy i w pełni przekonany, że chęć ciągłego rozwoju pozwoli 
         mi osiągnąć wyznaczony przeze mnie cel, czyli pracę na stanowisku Front-End Developera.`
-    
-        moreMobileParaAboutMe.classList.toggle('showTxt')
-    e.target.textContent == 'o mnie' ? e.target.textContent = 'ukryj' : e.target.textContent = 'o mnie'
-    }
-    if(e.target.matches('.btnMySkills')){
-        mobileLanguanges.classList.toggle('showTxt')
-        cytat.classList.toggle('hideTxt')
+     moreMobileParaAboutMe.classList.toggle('showTxt')
+     cytat.classList.toggle('hideTxt')
         signature.classList.toggle('hideTxt')
         btnShowCV.classList.toggle('hideTxt')
         btnGetCv.classList.toggle('hideTxt')
         btnMyTaks.classList.toggle('hideTxt')
-        e.target.textContent == 'moje skile </>' ? e.target.textContent = 'ukryj skile </>' : e.target.textContent = 'moje skile </>'
+        btnMySkills.classList.toggle('hideTxt')
+    e.target.textContent == 'o mnie' ? e.target.textContent = 'ukryj' : e.target.textContent = 'o mnie'
     }
 }
 `${moreMobileParaAboutMe.textContent}`
-const closeIcon = (e)=>{
-    if(e.target.matches('.closeFav')){
+
+// pokaż-zamknij skile
+const showSkills = e =>{
+    if(e.target.matches('.btnMySkills') || e.target.matches('.closeFavSkills')){
         mobileLanguanges.classList.toggle('showTxt')
         cytat.classList.toggle('hideTxt')
         signature.classList.toggle('hideTxt')
         btnShowCV.classList.toggle('hideTxt')
         btnGetCv.classList.toggle('hideTxt')
         btnMyTaks.classList.toggle('hideTxt')
+        btnReadMore.classList.toggle('hideTxt')
         btnMySkills.textContent == 'moje skile </>' ? btnMySkills.textContent = 'ukryj skile </>' : btnMySkills.textContent = 'moje skile </>'
     }
 }
@@ -108,13 +107,35 @@ const getCV  = (e) =>{
         download('./obrazy/dokumenty/Andrzej_Gębala_CV_IT.pdf', 'Andrzej_Gębala_CV')
     }
 }
+// pokaż CV
+let divImg = document.createElement('div')
+    divImg.style.height = 50 + 'vh'
+    let openModalCV = document.createElement('img')
+    openModalCV.setAttribute('src', './obrazy/dokumenty/Andrzej_Gebala_CV_IT_photo.jpg')
+    openModalCV.style.margin = '0 auto'
+    divImg.append(openModalCV)
+    divImg.classList.add('hideTxt')
+    titleContener.append(divImg)
+const showCV = e =>{
+     if(e.target.matches('.btnShowCV')){
+        divImg.classList.toggle('showTxt')
+        cytat.classList.toggle('hideTxt')
+        signature.classList.toggle('hideTxt')
+        btnMySkills.classList.toggle('hideTxt')
+        // btnGetCv.classList.toggle('hideTxt')
+        btnMyTaks.classList.toggle('hideTxt')
+        btnReadMore.classList.toggle('hideTxt')
+        btnShowCV.textContent == 'zobacz moje CV' ? btnShowCV.textContent = 'zamknij moje CV' : btnShowCV.textContent = 'zobacz moje CV'
+    }
+}
 
 
 
 
 
 
-mobileLanguanges.addEventListener('click', closeIcon)
+mobileAboutMeSection.addEventListener('click', showSkills)
 titleContener.addEventListener('click', showMoreTxt)
 titleContener.addEventListener('click', getCV)
+titleContener.addEventListener('click', showCV)
 mobileNavBtnContener.addEventListener('click', hideOpenMenu)
